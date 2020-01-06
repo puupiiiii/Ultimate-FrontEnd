5659737e5daf1c6a1875448f8fed25b60cd43a0d
HebaSalem <puupiiiii@gmail.com>
2020-01-06 17:29:16 +0200

    Lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,7 +13,45 @@ $(function () {
         
     });
 
+    //toggle fullscreen
+    $('.toggle-fullscreen').on('click',function (){
+        $(this).toggleClass('full-screen');
+        if($(this).hasClass('full-screen')) { // the page is fullscreen
+            openFullscreen(); 
+        } else { // the page isn't fullscreen
+            closeFullscreen();
+        }
+    });
 
+
+    /* Get the documentElement (<html>) to display the page in fullscreen */
+    var elem = document.documentElement;
+
+    /* View in fullscreen */
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox */
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            elem.msRequestFullscreen();
+        }
+    }
+
+    /* Close fullscreen */
+    function closeFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { /* Firefox */
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE/Edge */
+            document.msExitFullscreen();
+        }
+    }
 
 
 
